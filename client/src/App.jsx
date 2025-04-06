4722a79526741cf06f5e44185bf6e77dd4fefd7e
biubietnam <biub7596@gmail.com>
2025-04-06 19:11:59 +0700

    pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Homepage from './Components/Homepage';
+import Homepage from './pages/Homepage';
 import React from 'react';
 import './App.css';
 
@@ -7,33 +7,10 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <header>
-          <nav>
-            <ul>
-              <li><Link to="/">Home</Link></li>
-              <li><Link to="/about">About</Link></li>
-              <li><Link to="/contact">Contact</Link></li>
-            </ul>
-          </nav>
-        </header>
-
-        <body>
-          <main>
-            <Routes>
-              <Route path="/" element={<Homepage />} />
-              <Route path="/about" element={<h1>About Us</h1>} />
-              <Route path="/contact" element={<h1>Contact Us</h1>} />
-            </Routes>
-          </main>
-        </body>
-
-        <footer>
-          <p>&copy; 2023 Your Company</p>
-          <p>All rights reserved.</p>
-        </footer>
+        <Routes>
+          <Route></Route>
+        </Routes>
       </div>
     </Router>
   );
 }
-
-export default App;
