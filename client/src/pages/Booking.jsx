796ec57023a7872cfac29cdbc31413f8aa2d6a5c
Dante97vn <quocdatvan97@gmail.com>
2025-04-19 21:07:16 +0700

    Dat update booking

--- client/src/pages/Booking.jsx
+++ client/src/pages/Booking.jsx
@@ -2,23 +2,59 @@
 
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// Tạo ra array chứa object của 4 thẻ trang
+const cards = [
+  { title: "Vets", path: "/petVet", imgA: "/img/booking/active/petVet-a.png" },
+  { title: "Grooming", path: "/petGrooming", imgA: "/images/grooming.jpg" },
+  { title: "Events & Workshop", path: "/petEvent", imgA: "/images/events.jpg" },
+  { title: "Advice", path: "/petAdivce", imgA: "/images/advice.jpg" },
+];
 
 // viet hien thi hello world
 function Booking() {
   return (
-    <div style={{ display: "flex", gap: "10px", padding: "20px" }}>
-      <Link to="/petVet">
-        <button>Vets</button>
-      </Link>
-      <Link to="/petGrooming">
-        <button>Grooming</button>
-      </Link>
-      <Link to="/petEvent">
-        <button>Events & Workshop</button>
-      </Link>
-      <Link to="/petAdvice">
-        <button>Advice</button>
-      </Link>
+    <div>
+      <nav>
+        <ul className="nav nav-tabs ">
+          <li>
+            <button className="card ">
+              <img
+                src="/img/booking/active/petVet-a.png"
+                alt="vet"
+                className="card-img-top"
+                style={{ width: "180px", height: "180px" }}
+              ></img>
+              <div className="card-body">
+                <h5 className="card-title">Vets</h5>
+                <p className="card-text">Find the best vet for your pet.</p>
+                <Link to="/petVet" className="btn btn-primary">
+                  Book Now
+                </Link>
+              </div>
+            </button>
+          </li>
+
+          <li>
+            <button className="card ">
+              <img
+                src="/img/booking/active/petVet-a.png"
+                alt="vet"
+                className="card-img-top"
+                style={{ width: "180px", height: "180px" }}
+              ></img>
+              <div className="card-body">
+                <h5 className="card-title">Vets</h5>
+                <p className="card-text">Find the best vet for your pet.</p>
+                <Link to="/petVet" className="btn btn-primary">
+                  Book Now
+                </Link>
+              </div>
+            </button>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
