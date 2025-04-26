0b707f2d3e8f472f801db87dd932b5504d690a2f
Dante97vn <quocdatvan97@gmail.com>
2025-04-26 21:37:38 +0700

    Dat update booking

--- client/src/pages/Booking/petEvent.jsx
+++
@@ -1,9 +0,0 @@
-function PetEvent() {
-  return (
-    <div className="min-h-screen w-full px-4 sm:px-6 md:px-8 lg:px-16 xl:px-24 max-w-[1280px] mx-auto text-gray-700 py-20">
-      <h1>Pet Event</h1>
-      <p>Details about the pet event will go here.</p>
-    </div>
-  );
-}
-export default PetEvent;
