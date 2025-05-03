fd3c937a91db7b4c213f58bc65ca7706eaf6cc5d
Dante97vn <quocdatvan97@gmail.com>
2025-05-03 13:03:40 +0700

    Dat update complete frontend booking

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,8 +10,6 @@ import Homepage from "./pages/Homepage";
 
 //Booking page
 import Booking from "./pages/Booking";
-import PetHotel from "./pages/Booking/petHotel";
-import HotelBooking from "./pages/Booking/hotelBooking";
 
 import PetSpa from "./pages/Booking/petSpa";
 import PetVet from "./pages/Booking/petVet";
@@ -26,33 +24,31 @@ import PetAdoptionPage from "./pages/shop/Pet/PetAdoptionPage";
 
 export default function App() {
   return (
-      <div className="App font-concert bg-gray-100 min-h-screen">
-        <Header />
+    <div className="App font-concert bg-gray-100 min-h-screen">
+      <Header />
 
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/gearshop" element={<GearShop />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/product/:id" element={<ProductDetail />} />
-          <Route path="/booking" element={<Booking />} />
-          <Route path="/pethotel" element={<PetHotel />} />
-          <Route path="/hotelbooking" element={<HotelBooking />} />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/gearshop" element={<GearShop />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/product/:id" element={<ProductDetail />} />
+        <Route path="/booking" element={<Booking />} />
 
-          <Route path="/petspa" element={<PetSpa />} />
-          <Route path="/petvet" element={<PetVet />} />
-          <Route path="/reset/:token" element={<ResetPasswordFormContent />} />
-          <Route path="/login" element={<LoginFormContent />} />
-          <Route
-            path="/forgot-password"
-            element={<ForgotPasswordFormContent />}
-          />
-          <Route path="/signup" element={<SignUpFormContent />} />
-          <Route path="/admin/dashboard" element={<AdminDashboard />} />
-          <Route path="/admin/users" element={<Users />} />
-          <Route path="/petshop" element={<OurPets />} />
-          <Route path="/pet/:id" element={<PetAdoptionPage />} />
-        </Routes>
-        <Footer />
-      </div>
+        <Route path="/petspa" element={<PetSpa />} />
+        <Route path="/petvet" element={<PetVet />} />
+        <Route path="/reset/:token" element={<ResetPasswordFormContent />} />
+        <Route path="/login" element={<LoginFormContent />} />
+        <Route
+          path="/forgot-password"
+          element={<ForgotPasswordFormContent />}
+        />
+        <Route path="/signup" element={<SignUpFormContent />} />
+        <Route path="/admin/dashboard" element={<AdminDashboard />} />
+        <Route path="/admin/users" element={<Users />} />
+        <Route path="/petshop" element={<OurPets />} />
+        <Route path="/pet/:id" element={<PetAdoptionPage />} />
+      </Routes>
+      <Footer />
+    </div>
   );
 }
--- client/src/pages/Booking/petVet.jsx
+++ client/src/pages/Booking/petVet.jsx
@@ -12,10 +12,15 @@ function PetVet() {
         viewport={{ once: true, amount: 0.3 }}
       >
         <h2
-          className={`text-3xl md:text-4xl lg:text-5xl font-extrabold text-center text-[#6D7AB5] pb-10 mb-4 font-playfair tracking-wide drop-shadow-lg `}
+          className={`text-3xl md:text-4xl lg:text-5xl font-extrabold text-center text-[#6D7AB5] p-4  font-playfair tracking-wide drop-shadow-lg `}
         >
           🐾 Pet Healthcare Services at PetZone
         </h2>
+        <p
+          className={`text-base sm:text-lg md:text-xl text-center text-gray-800 max-w-3xl mx-auto leading-relaxed mb-10 font-lora `}
+        >
+          Comprehensive, compassionate care for every stage of your pet’s life.
+        </p>
       </motion.div>
 
       {/* Description about health service 6 service*/}
@@ -40,7 +45,7 @@ function PetVet() {
             up-to-date knowledge, we help detect issues early and support your
             pet’s long-term well-being.
           </p>
-          <button className="bg-yellow-100 text-yellow-700 hover:bg-yellow-200 active:bg-yellow-300 border border-yellow-300 font-bold text-lg py-3 px-6 rounded-full w-full transition-all duration-300 shadow-sm hover:shadow-md">
+          <button className="bg-[#6D7AB5] text-[#FFFFFF] hover:bg-[#4E5C8A] active:bg-[#4E5C8A] border border-[#4E5C8A] font-bold text-lg py-3 px-6 rounded-full w-full transition-all duration-300 shadow-sm hover:shadow-md">
             Book Now
           </button>
         </motion.div>
@@ -65,7 +70,7 @@ function PetVet() {
           initial={{ x: -100, opacity: 0 }} //trượt từ bên trái
           whileInView={{ x: 0, opacity: 1 }}
           transition={{ duration: 0.6 }}
-          viewport={{ once: true, amount: 0.3 }}
+          viewport={{ once: true, amount: 0.2 }}
         >
           <img
             src="/img/booking/vet/petCareHome.png"
@@ -78,7 +83,7 @@ function PetVet() {
           initial={{ x: 100, opacity: 0 }} //trượt từ bên phải
           whileInView={{ x: 0, opacity: 1 }}
           transition={{ duration: 0.6 }}
-          viewport={{ once: true, amount: 0.3 }}
+          viewport={{ once: true, amount: 0.2 }}
         >
           <h3 className="text-xl md:text-2xl lg:text-3xl text-[#6D7AB5] font-semibold mb-6">
             🏠 PetZone Vet at Home
@@ -92,68 +97,201 @@ function PetVet() {
             we provide professional care for your pets in the comfort of your
             own home, with modern equipment and gentle handling.
           </p>
-          <button className="bg-yellow-100 text-yellow-700 hover:bg-yellow-200 active:bg-yellow-300 border border-yellow-300 font-bold text-lg py-3 px-6 rounded-full w-full transition-all duration-300 shadow-sm hover:shadow-md">
+          <button className="bg-[#6D7AB5] text-[#FFFFFF] hover:bg-[#4E5C8A] active:bg-[#4E5C8A] border border-[#4E5C8A] font-bold text-lg py-3 px-6 rounded-full w-full transition-all duration-300 shadow-sm hover:shadow-md">
             Book Now
           </button>
         </motion.div>
       </div>
 
       {/* Description about tiem phong thu cung*/}
-      <motion.div
-        initial={{ x: -100, opacity: 0 }} //trượt từ bên trái
-        whileInView={{ x: 0, opacity: 1 }}
-        transition={{ duration: 0.6 }}
-        viewport={{ once: true, amount: 0.3 }}
-      ></motion.div>
+      <div className="grid sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1 xl:grid-cols-2 justify-items-center mt-10 ">
+        <motion.div
+          initial={{ x: -100, opacity: 0 }} //trượt từ bên trái
+          whileInView={{ x: 0, opacity: 1 }}
+          transition={{ duration: 0.6 }}
+          viewport={{ once: true, amount: 0.3 }}
+        >
+          <h3 className="text-xl md:text-2xl lg:text-3xl text-[#6D7AB5] font-semibold mb-6">
+            💉 PetZone Immunization Care
+          </h3>
+          <p className="text-base md:text-base lg:text-xl text-[#6D7AB5] font-semibold mb-6">
+            Shield your pet from illness with vaccines.
+          </p>
+          <p className="text-gray-600 text-base md:text-lg lg:text-xl mb-6 leading-relaxed">
+            At PetZone, we provide a full range of vaccines tailored to your
+            pet’s age, lifestyle, and health condition. All vaccines are stored
+            and administered under strict standards to ensure maximum
+            protection. Our team offers gentle handling and clear guidance,
+            making the vaccination process smooth and stress-free — for both you
+            and your pet.
+          </p>
+          <button className="bg-[#6D7AB5] text-[#FFFFFF] hover:bg-[#4E5C8A] active:bg-[#4E5C8A] border border-[#4E5C8A] font-bold text-lg py-3 px-6 rounded-full w-full transition-all duration-300 shadow-sm hover:shadow-md">
+            Book Now
+          </button>
+        </motion.div>
 
-      <motion.div
-        initial={{ x: 100, opacity: 0 }} //trượt từ bên phải
-        whileInView={{ x: 0, opacity: 1 }}
-        transition={{ duration: 0.6 }}
-        viewport={{ once: true, amount: 0.3 }}
-      ></motion.div>
+        <motion.div
+          initial={{ x: 100, opacity: 0 }} //trượt từ bên phải
+          whileInView={{ x: 0, opacity: 1 }}
+          transition={{ duration: 0.6 }}
+          viewport={{ once: true, amount: 0.3 }}
+        >
+          <img
+            src="/img/booking/vet/petVaccine.png"
+            alt="Pet Vaccine"
+            className=" sm:w-[300px]  md:w-[400px] h-auto lg:w-[500px] h-auto "
+          />
+        </motion.div>
+      </div>
+
       {/* Description about sieu am thu cung*/}
-      <motion.div
-        initial={{ x: -100, opacity: 0 }} //trượt từ bên trái
-        whileInView={{ x: 0, opacity: 1 }}
-        transition={{ duration: 0.6 }}
-        viewport={{ once: true, amount: 0.3 }}
-      ></motion.div>
+      <div className="grid sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1 xl:grid-cols-2 justify-items-center mt-10 ">
+        <motion.div
+          initial={{ x: -100, opacity: 0 }} //trượt từ bên trái
+          whileInView={{ x: 0, opacity: 1 }}
+          transition={{ duration: 0.6 }}
+          viewport={{ once: true, amount: 0.3 }}
+        >
+          <img
+            src="/img/booking/vet/petScan.png"
+            alt="Pet Scan"
+            className=" sm:w-[300px]  md:w-[400px] h-auto lg:w-[500px] h-auto "
+          />
+        </motion.div>
 
-      <motion.div
-        initial={{ x: 100, opacity: 0 }} //trượt từ bên phải
-        whileInView={{ x: 0, opacity: 1 }}
-        transition={{ duration: 0.6 }}
-        viewport={{ once: true, amount: 0.3 }}
-      ></motion.div>
+        <motion.div
+          initial={{ x: 100, opacity: 0 }} //trượt từ bên phải
+          whileInView={{ x: 0, opacity: 1 }}
+          transition={{ duration: 0.6 }}
+          viewport={{ once: true, amount: 0.3 }}
+        >
+          {" "}
+          <h3 className="text-xl md:text-2xl lg:text-3xl text-[#6D7AB5] font-semibold mb-6">
+            🩻 PetZone 4D Ultrasound Scan
+          </h3>
+          <p className="text-base md:text-base lg:text-xl text-[#6D7AB5] font-semibold mb-6">
+            Advanced imaging for accurate, non-invasive diagnosis.
+          </p>
+          <p className="text-gray-600 text-base md:text-lg lg:text-xl mb-6 leading-relaxed">
+            At PetZone, we use 4D ultrasound technology with high-resolution HD
+            probes and specialized veterinary software to deliver detailed
+            internal images of your pet. Whether it’s pregnancy monitoring,
+            abdominal issues, or internal health checks, our scans help detect
+            problems early — safely and comfortably. Quick, gentle, and highly
+            informative — for peace of mind and better treatment planning.
+          </p>
+          <button className="bg-[#6D7AB5] text-[#FFFFFF] hover:bg-[#4E5C8A] active:bg-[#4E5C8A] border border-[#4E5C8A] font-bold text-lg py-3 px-6 rounded-full w-full transition-all duration-300 shadow-sm hover:shadow-md">
+            Book Now
+          </button>
+        </motion.div>
+      </div>
+
       {/* Description about triet san thu cung*/}
-      <motion.div
-        initial={{ x: -100, opacity: 0 }} //trượt từ bên trái
-        whileInView={{ x: 0, opacity: 1 }}
-        transition={{ duration: 0.6 }}
-        viewport={{ once: true, amount: 0.3 }}
-      ></motion.div>
+      <div className="grid sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1 xl:grid-cols-2 justify-items-center mt-10 ">
+        <motion.div
+          initial={{ x: -100, opacity: 0 }} //trượt từ bên trái
+          whileInView={{ x: 0, opacity: 1 }}
+          transition={{ duration: 0.6 }}
+          viewport={{ once: true, amount: 0.3 }}
+        >
+          <h3 className="text-xl md:text-2xl lg:text-3xl text-[#6D7AB5] font-semibold mb-6">
+            🐾 PetZone Spay & Neuter Care
+          </h3>
+          <p className="text-base md:text-base lg:text-xl text-[#6D7AB5] font-semibold mb-6">
+            Gentle, safe sterilization for a healthier and happier life.
+          </p>
+          <p className="text-gray-600 text-base md:text-lg lg:text-xl mb-6 leading-relaxed">
+            Spaying or neutering your pet is a responsible choice that supports
+            long-term health, prevents certain diseases, and can reduce unwanted
+            behaviors. At PetZone, we provide safe and compassionate care
+            through every stage — from consultation to recovery. Our veterinary
+            team follows trusted procedures and uses modern equipment to ensure
+            comfort and safety for your furry friend.
+          </p>
+          <button className="bg-[#6D7AB5] text-[#FFFFFF] hover:bg-[#4E5C8A] active:bg-[#4E5C8A] border border-[#4E5C8A] font-bold text-lg py-3 px-6 rounded-full w-full transition-all duration-300 shadow-sm hover:shadow-md">
+            Book Now
+          </button>
+        </motion.div>
 
-      <motion.div
-        initial={{ x: 100, opacity: 0 }} //trượt từ bên phải
-        whileInView={{ x: 0, opacity: 1 }}
-        transition={{ duration: 0.6 }}
-        viewport={{ once: true, amount: 0.3 }}
-      ></motion.div>
+        <motion.div
+          initial={{ x: 100, opacity: 0 }} //trượt từ bên phải
+          whileInView={{ x: 0, opacity: 1 }}
+          transition={{ duration: 0.6 }}
+          viewport={{ once: true, amount: 0.3 }}
+        >
+          <img
+            src="/img/booking/vet/petSpray.png"
+            alt="Pet Spray"
+            className=" sm:w-[300px]  md:w-[400px] h-auto lg:w-[500px] h-auto "
+          />
+        </motion.div>
+      </div>
       {/* Description about tai sao chon PetZone*/}
-      <motion.div
-        initial={{ x: -100, opacity: 0 }} //trượt từ bên trái
-        whileInView={{ x: 0, opacity: 1 }}
-        transition={{ duration: 0.6 }}
-        viewport={{ once: true, amount: 0.3 }}
-      ></motion.div>
+      <div className="grid sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1 xl:grid-cols-2 justify-items-center mt-10 ">
+        <motion.div
+          initial={{ x: -100, opacity: 0 }} //trượt từ bên trái
+          whileInView={{ x: 0, opacity: 1 }}
+          transition={{ duration: 0.6 }}
+          viewport={{ once: true, amount: 0.3 }}
+        >
+          <img
+            src="/img/booking/vet/petZoneChoose.png"
+            alt="PetWhyChoosePetZone"
+            className=" sm:w-[300px]  md:w-[400px] h-auto lg:w-[500px] h-auto "
+          />
+        </motion.div>
 
-      <motion.div
-        initial={{ x: 100, opacity: 0 }} //trượt từ bên phải
-        whileInView={{ x: 0, opacity: 1 }}
-        transition={{ duration: 0.6 }}
-        viewport={{ once: true, amount: 0.3 }}
-      ></motion.div>
+        <motion.div
+          initial={{ x: 100, opacity: 0 }} //trượt từ bên phải
+          whileInView={{ x: 0, opacity: 1 }}
+          transition={{ duration: 0.6 }}
+          viewport={{ once: true, amount: 0.3 }}
+        >
+          <h3 className="text-xl md:text-2xl lg:text-3xl text-[#6D7AB5] font-semibold mb-6">
+            ❤️ Why Choose PetZone?
+          </h3>
+          <p className="text-base md:text-base lg:text-xl text-[#6D7AB5] font-semibold mb-6">
+            Trusted care, advanced technology, and a whole lot of love.
+          </p>
+          <p className="text-gray-600 text-base md:text-lg lg:text-sl mb-6 leading-relaxed">
+            <ul>
+              {" "}
+              <li>
+                🩺 <strong>Experienced Veterinarians</strong> – A dedicated team
+                that truly cares.
+              </li>{" "}
+              <li>
+                🧪 <strong>Modern Equipment</strong> – From 4D ultrasound to
+                in-home care tools.
+              </li>{" "}
+              <li>
+                🐾 <strong>Full-Service Clinic</strong> – Check-ups, vaccines,
+                grooming, surgery & more.
+              </li>{" "}
+              <li>
+                🚪 <strong>Convenient Options</strong> – On-site or at-home,
+                we’re here when you need us.
+              </li>{" "}
+              <li>
+                💬 <strong>Clear Communication</strong> – We guide you through
+                every step.
+              </li>{" "}
+              <li>
+                🐶 <strong>Gentle Approach</strong> – Stress-free care for pets
+                of all ages.
+              </li>{" "}
+            </ul>{" "}
+            <p>
+              <em>
+                Because your pet deserves not just care—but PetZone-level care.
+              </em>
+            </p>
+          </p>
+          <button className="bg-[#6D7AB5] text-[#FFFFFF] hover:bg-[#4E5C8A] active:bg-[#4E5C8A] border border-[#4E5C8A] font-bold text-lg py-3 px-6 rounded-full w-full transition-all duration-300 shadow-sm hover:shadow-md">
+            Book Now
+          </button>
+        </motion.div>
+      </div>
     </div>
   );
 }
